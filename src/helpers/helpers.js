f24cc5b4c223428274b1163fd489742cd1acadda
GRI-CAMP <gri-camp@yandex.ru>
2025-11-20 19:32:44 +0300

    1st_commit_deploy

--- src/helpers/helpers.js
+++
@@ -1,5 +0,0 @@
-const draw = (container, html) => container.insertAdjacentHTML("beforeend", html);
-
-const getHTMLFromList = (list, callback) => list.map(callback).join("");
-
-export { draw, getHTMLFromList };
